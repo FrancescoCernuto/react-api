998039bdb65fcc2c15252757002e98a06ce58fca
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-05 16:22:23 +0100

    output

--- vite-project/src/assets/components/Badge.jsx
+++ vite-project/src/assets/components/Badge.jsx
@@ -2,3 +2,6 @@ import { useState } from "react";
 
 export default function Badge({ badge, retrieveTags }) {
     const [tags, setTags] = useState([]);
+
+    function handleChange(e) {
+        const value = e.target.id;
