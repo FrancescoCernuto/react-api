fda35cae2134bbc0b706d34f6c2d82dc802a6e91
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-05 15:43:33 +0100

    export main w/alert

---
+++ vite-project/src/assets/components/layout/Main.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import CardList from "../CardList";
+
+
+const formInitialData = {
+    author: "",
+    content: "",
+    isPublic: false,
+    tags: [],
+    title: "",
+    category: "",
+};
+
+export default function Main() {
+    const [formData, setFormData] = useState(formInitialData);
+    const [cardList, setCardList] = useState([...posts]);
+    useEffect(() => {
+        if (formData.isPublic) alert("Public Post!");
+    }, [formData.isPublic]);
