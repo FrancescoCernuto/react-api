9e22aa57658695c36767495884d195a43b18c733
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-05 16:40:36 +0100

    create element for card

--- vite-project/src/assets/components/Card.jsx
+++ vite-project/src/assets/components/Card.jsx
@@ -1,1 +1,9 @@
-export default function Card({ card, handleClick }) { }
+export default function Card({ card, handleClick }) {
+    return (<>
+        <div className="card p-0 m-2">
+            <img
+                src="https://placehold.co/600x400"
+                className="card-img-top"
+                alt="..."
+            />
+ }
