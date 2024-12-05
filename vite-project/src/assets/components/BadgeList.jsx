b28c115fb3e7f3b6e7fc9fb4f5dcef099fe4adab
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-05 16:34:48 +0100

    create element html for badges

--- vite-project/src/assets/components/BadgeList.jsx
+++ vite-project/src/assets/components/BadgeList.jsx
@@ -1,2 +1,10 @@
 import Badge from "./Badge";
-export default function BadgeList({ badgeList, retrieveTags }) 
+export default function BadgeList({ badgeList, retrieveTags }) {
+    return (
+        <>
+            {badgeList.map((badge) => (
+                <Badge key={badge} badge={badge} retrieveTags={retrieveTags} />
+            ))}
+        </>
+    );
+}
