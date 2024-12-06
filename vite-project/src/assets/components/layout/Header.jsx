5b30fe5eef26aa0a68fa89479b9869366bedcaa9
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-06 15:42:33 +0100

    fix

--- vite-project/src/assets/components/layout/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-    return (
-        <header className="text-bg-dark border-1 border-bottom py-3">
-            <h1 className="text-center">React Form Multifield</h1>
-        </header>
-    );
-}
